2776b877c71c9aba83601766c3798eb6763f6392
Lily Flavelle <me@neurario.com>
2024-03-03 13:27:41 +1100

    Will come back to plasma6 later

--- hosts/snatcher.nix
+++ hosts/snatcher.nix
@@ -57,7 +57,7 @@
 
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  # services.xserver.desktopManager.plasma6.enable = true;
 
   services.preload.enable = true;
 
