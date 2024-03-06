6cbcd15defe70461d22c4adcb3dd6a9765309161
Lily Flavelle <me@neurario.com>
2024-03-07 00:54:19 +1100

    Gotta admit I'm not used to chromebook keyboards

--- flake.nix
+++ flake.nix
@@ -69,7 +69,7 @@
 
           ];
         };
-        "dweller" - nixpkgs.lib.nixosSystem {
+        "dweller" = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs; };
           modules = [
