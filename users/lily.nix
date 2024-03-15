871817b061488292ce3624d3475309d310314b50
Lily Flavelle <me@neurario.com>
2024-03-15 14:58:49 +1100

    hyprlock config feat packages

--- users/lily.nix
+++ users/lily.nix
@@ -46,10 +46,12 @@ in {
             withOpenASAR = !lowPower;
             withVencord = !lowPower;
           })
+          gtkcord4
           playerctl
           pavucontrol
           bitwarden
           rclone
+          fontpreview
 
           tetrio-desktop
 
@@ -233,12 +235,16 @@ in {
         enable = true;
         userName = "Lily Flavelle";
         userEmail = "me@neurario.com";
+        diff-so-fancy.enable = true;
+        extraConfig= {
+            credential.helper = "store";
+        };
       };
 
       qt.enable = graphical;
 
       services.darkman = {
-        enable = true;
+        enable = false;
         settings = {
           lat = 36.76;
           lng = 144.29;
@@ -250,18 +256,10 @@ in {
 
       services.playerctld.enable = true;
 
-      wayland.windowManager.sway = {
-          enable = lowPower;
-          config = rec {
-              modifier = "Mod4"; # Search/Logo on Chromebook
-              terminal = "foot";
-          };
-      };
-
 			xdg.configFile."hypr/hypridle.conf".text = ''
 				general {
     				lock_cmd = pidof hyprlock || hyprlock
-    				unlock_cmd = loginctl unlock-session
+    				unlock_cmd = kill -USR1 $(pidof hyprlock)
     				before_sleep_cmd = loginctl lock-session
     				after_sleep_cmd = hyprctl dispatch dpms on
 				}
@@ -276,6 +274,57 @@ in {
     				on-timeout = hyprctl dispatch dpms off
     				on-resume = hyprctl dispatch dpms on
 				}
+			'';
+			xdg.configFile."hypr/hyprlock.conf".text = ''
+				general {
+    				grace = 30
+				}
+
+				background {
+    				monitor =
+    				path = screenshot
+
+    				blur_passes = 3
+    				blur_size = 10
+				}
+
+				input-field {
+				    monitor =
+				    size = 200, 50
+				    outline_thickness = 3
+				    dots_size = 0.33 # Scale of input-field height, 0.2 - 0.8
+				    dots_spacing = 0.15 # Scale of dots' absolute size, 0.0 - 1.0
+				    dots_center = false
+				    dots_rounding = -1 # -1 default circle, -2 follow input-field rounding
+				    outer_color = rgb(151515)
+				    inner_color = rgb(200, 200, 200)
+				    font_color = rgb(10, 10, 10)
+				    fade_on_empty = true
+				    fade_timeout = 1000 # Milliseconds before fade_on_empty is triggered.
+				    placeholder_text = <i>Input Password...</i> # Text rendered in the input box when it's empty.
+				    hide_input = false
+				    rounding = -1 # -1 means complete rounding (circle/oval)
+				    check_color = rgb(204, 136, 34)
+				    fail_color = rgb(204, 34, 34) # if authentication failed, changes outer_color and fail message color
+				    fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i> # can be set to empty
+				    fail_transition = 300 # transition time in ms between normal outer_color and fail_color
+				    capslock_color = -1
+				    numlock_color = -1
+				    bothlock_color = -1 # when both locks are active. -1 means don't change outer color (same for above)
+				    invert_numlock = false # change color if numlock is off
+
+				    position = 0, -20
+				    halign = center
+				    valign = center
+				}
+
+				label {
+    				monitor =
+    				text = cmd[update:1000] echo "<span foreground='##ff2222'>$(date)</span>"
+    				font_size = 24
+    				font_family = "xkcd-Regular"
+				}
+
 			'';
 			xdg.configFile."hypr/hyprpaper.conf".text = ''
 				splash = false
