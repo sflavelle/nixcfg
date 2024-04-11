9e9838b98daf443069fd54fc0d11448065813aeb
Lily Flavelle <me@neurario.com>
2024-04-11 22:41:26 +1000

    rquickshare: desktop item

--- pkgs/rquickshare.nix
+++ pkgs/rquickshare.nix
@@ -1,11 +1,24 @@
 { lib
-, pkgs
+, appimageTools
+, copyDesktopItems
+, makeDesktopItem
 }:
     pkgs.appimageTools.wrapType2 {
         name = "r-quick-share";
         version = "0.5.0";
         src = pkgs.fetchurl {
            url = "https://github.com/Martichou/rquickshare/releases/download/v0.5.0/r-quick-share_0.5.0_amd64_GLIBC-2.31.AppImage";
-           hash = "";
+           hash = "sha256-ilVXMyPAxn58YNiMY2Q8PAasYe7XSq01Mj8i458VvsY=";
         };
-    };
+
+        desktopItems = [
+            (makeDesktopItem {
+                name = "r-quick-share";
+                exec = "r-quick-share";
+                icon = "r-quick-share";
+                desktopName = "Quick Share";
+                genericName = "Android file sharing";
+                categories = "Utilities;Internet;";
+            })
+        ];
+    }
