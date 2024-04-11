4c5f75cb15b6ee793295e05a2d2b9f5f21f61059
Lily Flavelle <me@neurario.com>
2024-04-11 22:43:46 +1000

    rquickshare: strip pkgs prefix

--- pkgs/rquickshare.nix
+++ pkgs/rquickshare.nix
@@ -1,12 +1,13 @@
 { lib
 , appimageTools
+, fetchurl
 , copyDesktopItems
 , makeDesktopItem
 }:
-    pkgs.appimageTools.wrapType2 {
+    appimageTools.wrapType2 {
         name = "r-quick-share";
         version = "0.5.0";
-        src = pkgs.fetchurl {
+        src = fetchurl {
            url = "https://github.com/Martichou/rquickshare/releases/download/v0.5.0/r-quick-share_0.5.0_amd64_GLIBC-2.31.AppImage";
            hash = "sha256-ilVXMyPAxn58YNiMY2Q8PAasYe7XSq01Mj8i458VvsY=";
         };
