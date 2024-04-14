890a9d7eccd828193814f03ba087f11480b05d15
Lily Flavelle <me@neurario.com>
2024-04-14 22:18:03 +1000

    quick hack for obs plugin advanced masks

---
+++ pkgs/obs-plugin-advanced-masks.nix
@@ -0,0 +1,41 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, obs-studio
+, cmake
+}:
+
+stdenv.mkDerivation rec {
+  pname = "obs-advanced-masks";
+  version = "1.1.0";
+
+  src = fetchFromGitHub {
+    owner = "FiniteSingularity";
+    repo = "obs-advanced-masks";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-icn0X+c7Uf0nTFaVDVTPi26sfWTSeoAj7+guEn9gi9Y=";
+  };
+
+  buildInputs = [
+    obs-studio
+  ];
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  postInstall = ''
+    rm -rf "$out/share"
+    mkdir -p "$out/share/obs"
+    mv "$out/data/obs-plugins" "$out/share/obs"
+    rm -rf "$out/obs-plugins" "$out/data"
+  '';
+
+  meta = with lib; {
+    description = "An open-source project designed to expand the masking functionalities within OBS Studio";
+    homepage = "https://github.com/FiniteSingularity/obs-advanced-masks";
+    license = licenses.gpl2Only;
+    mainProgram = "obs-advanced-masks";
+    platforms = platforms.linux;
+  };
+}
