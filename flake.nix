a2da8cb9ee6ddd5128cf4c286e04c0214b9cb79a
Lily Flavelle <me@neurario.com>
2025-04-11 23:01:59 +1000

    nuke old configuration to a basic state

--- flake.nix
+++ flake.nix
@@ -12,26 +12,19 @@
     # which represents the GitHub repository URL + branch/commit-id/tag.
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nix-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nix-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    disko = {
-        url = "github:nix-community/disko";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
     nixos-generators = {
         url = "github:nix-community/nixos-generators";
         inputs.nixpkgs.follows = "nixpkgs";
     };
-    musnix.url = "github:musnix/musnix";
-    stylix.url = "github:danth/stylix";
-    sops-nix.url = "github:Mic92/sops-nix";
-    bizhawk.url = "github:TASEmulators/BizHawk/master";
-    bizhawk.flake = false;
     mac-brcm-fw = {
         url = "github:AdityaGarg8/Apple-Firmware";
         flake = false;
+    };
+    jovian = {
+	url = "github:Jovian-Experiments/Jovian-NixOS";
+	inputs.nixpkgs.follows = "nixpkgs";
     };
 
   };
@@ -41,65 +34,35 @@
       nixpkgs,
       nix-stable,
       nixos-hardware,
-      disko,
       nixos-generators,
-      musnix,
-      home-manager,
-      mac-brcm-fw,
-      stylix,
-      sops-nix,
-      bizhawk,
       ... }@inputs:
-				let
-					system = "x86_64-linux";
-					overlay-stable = final: prev: {
+	let
+	  system = "x86_64-linux";
+	  overlay-stable = final: prev: {
             stable = import nix-stable { inherit system; config.allowUnfree = true; };
-					};
-
-				in
+	  };
+	in
       {
       nixosModules."commonModules" = { config, lib, inputs, ... }:{
         imports = [
-          inputs.home-manager.nixosModules.home-manager
-          inputs.stylix.nixosModules.stylix
-          inputs.sops-nix.nixosModules.sops
         ];
 
         nixpkgs.overlays = [ overlay-stable ];
-
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          extraSpecialArgs = { inherit inputs; };
-        };
-
-        sops = {
-          defaultSopsFile = ./secrets/secrets.yaml;
-          age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-          age.keyFile = "/home/lily/.config/sops/age/keys.txt";
-          age.generateKey = true;
-        };
 
         nix.settings = {
           substituters = [];
           trusted-public-keys = [];
         };
-				nixpkgs.config.permittedInsecurePackages = [
-                "openssl-1.1.1w"
+	nixpkgs.config.permittedInsecurePackages = [
         ];
       };
       nixosConfigurations = {
         "snatcher" = nixpkgs.lib.nixosSystem {
           # Primary Desktop PC
           system = "x86_64-linux";
-          specialArgs = {
-            inherit inputs;
-          }; # pass custom arguments into all sub module.
+          specialArgs = { inherit inputs; };
           modules = [
             ./hosts/snatcher.nix
-            ./common/desktop.nix
-            ./users/lily.nix
-            musnix.nixosModules.musnix
             self.nixosModules.commonModules
           ];
         };
@@ -109,23 +72,26 @@
           specialArgs = { inherit inputs; };
           modules = [
             ./hosts/minion.nix
-            ./common/desktop.nix
-            ./users/lily.nix
             self.nixosModules.commonModules
 
           ];
+        };
+        "empress" = nixpkgs.lib.nixosSystem {
+	  # Lenovo Legion Go
+	  system = "x86_64-linux";
+	  specialArgs = { inherit inputs; };
+	  modules = [
+	    ./hosts/empress.nix
+	    self.nixosModules.commonModules
+	    inputs.jovian.nixosModules.default
+	  ];
         };
         "dweller" = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs; };
           modules = [
             ./hosts/dweller.nix
-            ./common/desktop.nix
-            ./users/lily.nix
             self.nixosModules.commonModules
-
-            disko.nixosModules.disko
-            ./fragments/dweller-disk.nix
           ];
         };
         "badgeseller" = nixpkgs.lib.nixosSystem {
@@ -133,14 +99,9 @@
             specialArgs = { inherit inputs; };
             modules = [
                 ./hosts/badgeseller.nix
-                ./common/desktop.nix
-                ./users/lily.nix
                 self.nixosModules.commonModules
                 nixos-hardware.nixosModules.apple-macbook-air-6
                 nixos-hardware.nixosModules.apple-t2
-
-                disko.nixosModules.disko
-                ./fragments/badgeseller-disk.nix
             ];
         };
         "rumbi" = nixpkgs.lib.nixosSystem {
@@ -154,43 +115,5 @@
                 ./hosts/rumbi.nix
             ];
         };
-        # Servers
-        "neurariodotcom" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./hosts/neurariodotcom.nix
-            ./common/server.nix
-            ./users/lily.nix
-            self.nixosModules.commonModules
-          ];
-        };
-        "conductor" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./hosts/conductor.nix
-            ./common/server.nix
-            ./users/lily.nix
-            self.nixosModules.commonModules
-          ];
-        };
-      };
-      packages.x86_64-linux = {
-        # Utilities
-        "isopadre" = nixos-generators.nixosGenerate { # Generic ISO image
-        	system = "x86_64-linux";
-        	specialArgs = { inherit inputs; };
-        	format = "install-iso";
-        	modules = [
-            	./common/desktop.nix
-            	./users/lily.nix
-            	self.nixosModules.commonModules
-            	{
-                	networking.networkmanager.enable = false;
-            	}
-        	];
-      	};
-      };
     };
 }
--- hosts/minion.nix
+++ hosts/minion.nix
@@ -2,93 +2,151 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  imports = [ # Include the results of the hardware scan.
-    ../hardware/minion.nix
-  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Seems my patch is built for 6.6
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_6;
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.loader.grub = {
-    enable = true;
-    efiSupport = true;
-    device = "nodev";
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "empress"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Australia/Melbourne";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
   };
 
-  boot.kernelPatches = [{
-    name = "acpi quirk";
-    patch = pkgs.fetchurl {
-        url = "https://git.neurario.com/splatsune/nixcfg/raw/commit/87c4f04fcdc8564982fe780aa25024a54c5dada4/hardware/minion-fixirq.patch";
-        hash = "sha256-Lo4QWOC2sVRsB+90uiaimuyhFXyaHgKuAeDLN+Bjb/g=";
-    };
-  }];
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
 
-  networking.hostName = "minion"; # Define your hostname.
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
-  services.tlp = { enable = true; };
-  services.power-profiles-daemon.enable =
-    lib.mkForce false; # Since TLP is enabled instead
-
-  hardware.nvidia.prime = {
-    amdgpuBusId = "PCI:5:0:0";
-    nvidiaBusId = "PCI:1:0:0";
-    offload.enable = true;
-    offload.enableOffloadCmd = true;
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "au";
+    variant = "";
   };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  services.printing.drivers = with pkgs; [ hplip ];
 
-	hardware.bluetooth = {
-    	enable = true;
-	};
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  users.users.lily.extraGroups = [ "audio" ];
-  users.users.lily.packages = (with pkgs; [
-    pandoc
-    # Programs
-    steam-rom-manager
-    lutris
-
-    # Custom Packages
-    (pkgs.callPackage ../pkgs/poptracker.nix { })
-
-  ]);
-
-  services.syncthing = {
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    openDefaultPorts = true;
-    user = "lily";
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
-  virtualisation.podman.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.splatsune = {
+    isNormalUser = true;
+    description = "Simon Flavelle";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate gh git steam-run
+      thunderbird
+      bitwarden-desktop
+      mpv
+      jetbrains.pycharm-community-src
+      (discord.override {
+      withOpenASAR = true;
+      withVencord = true;
+    })
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+  programs.localsend.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    protontricks.enable = true;
+    gamescopeSession.enable = true;
+    
+  };
+  programs.gamescope.enable = true;
+  services.tailscale.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    cfs-zen-tweaks
-    wget
-    httpie
-    pandoc
-    powershell
-    git
-    curl
-    partition-manager
-    gparted
-    wineWowPackages.stable
-    coreutils
-    clang
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
   ];
 
-  system.stateVersion = "23.05"; # Did you read the comment?
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
