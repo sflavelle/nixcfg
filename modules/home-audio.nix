867186637de73a95faeac771b0c034c6cdd8c243
Lily Flavelle <me@neurario.com>
2025-05-05 01:00:52 +1000

    these 'radio' streams aren't working right now for some reason

--- modules/home-audio.nix
+++ modules/home-audio.nix
@@ -25,34 +25,34 @@
         sampleFormat = "48000:16:2";
         codec = "pcm";
       };
-      "Soundscapes" = {
-        type = "process";
-        location = "${pkgs.mpv}/bin/mpv";
-        sampleFormat = "48000:16:2";
-        query.logStderr = "true";
-        query.params = lib.strings.concatStringsSep " " [
-          "/mnt/media/soundscapes"
-          "--shuffle --volume=0.5"
-          "--no-terminal --audio-display=no --audio-channels=stereo"
-          "--audio-samplerate=48000 --audio-format=s16"
-          "--af=lavfi=[dynaudnorm=f=75:g=201:p=0.55:s=10]"
-          "--ao=pcm --ao-pcm-file=/dev/stdout"
-        ];
-      };
-      "ABC NewsRadio" = {
-        type = "process";
-        location = "${pkgs.mpv}/bin/mpv";
-        sampleFormat = "48000:16:2";
-        query.logStderr = "true";
-        query.params = lib.strings.concatStringsSep " " [
-          "https://mediaserviceslive.akamaized.net/hls/live/2038311/newsradio/index.m3u8"
-          "--shuffle --volume=1.0"
-          "--no-terminal --audio-display=no --audio-channels=stereo"
-          "--audio-samplerate=48000 --audio-format=s16"
-          "--af=lavfi=[dynaudnorm=f=75:g=201:p=0.55:s=10]"
-          "--ao=pcm --ao-pcm-file=/dev/stdout"
-        ];
-      };
+      # "Soundscapes" = {
+      #   type = "process";
+      #   location = "${pkgs.mpv}/bin/mpv";
+      #   sampleFormat = "48000:16:2";
+      #   query.logStderr = "true";
+      #   query.params = lib.strings.concatStringsSep " " [
+      #     "/mnt/media/soundscapes"
+      #     "--shuffle --volume=0.5"
+      #     "--no-terminal --audio-display=no --audio-channels=stereo"
+      #     "--audio-samplerate=48000 --audio-format=s16"
+      #     "--af=lavfi=[dynaudnorm=f=75:g=201:p=0.55:s=10]"
+      #     "--ao=pcm --ao-pcm-file=/dev/stdout"
+      #   ];
+      # };
+      # "ABC NewsRadio" = {
+      #   type = "process";
+      #   location = "${pkgs.mpv}/bin/mpv";
+      #   sampleFormat = "48000:16:2";
+      #   query.logStderr = "true";
+      #   query.params = lib.strings.concatStringsSep " " [
+      #     "https://mediaserviceslive.akamaized.net/hls/live/2038311/newsradio/index.m3u8"
+      #     "--shuffle --volume=1.0"
+      #     "--no-terminal --audio-display=no --audio-channels=stereo"
+      #     "--audio-samplerate=48000 --audio-format=s16"
+      #     "--af=lavfi=[dynaudnorm=f=75:g=201:p=0.55:s=10]"
+      #     "--ao=pcm --ao-pcm-file=/dev/stdout"
+      #   ];
+      # };
     };
   };
 }
