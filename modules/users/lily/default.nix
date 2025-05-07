955853222cc4f2d0fb133d030d77ad2692c72175
Lily Flavelle <me@neurario.com>
2025-05-07 23:35:29 +1000

    add lily to trusted-users

--- modules/users/lily/default.nix
+++ modules/users/lily/default.nix
@@ -4,7 +4,7 @@ let
   name = if config.hostSpec.isPublic then "Simon Flavelle" else "Lily Flavelle"; # yes i'm not out yet
 in
 {
-
+  nix.settings.trusted-users = [ mainUser ];
   users.users."${mainUser}" = {
     isNormalUser = true;
     initialHashedPassword = "$y$j9T$YtsEpAHJxRS/EyGrxjdC3.$p5EZjR9.344Xu2kVHyB.RLLGLetkSD/oT1Me8UbX3x4";
