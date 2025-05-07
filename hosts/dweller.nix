21fc824cf5e51cccf9521ec6662ea9db6695a2f8
Lily Flavelle <me@neurario.com>
2025-05-07 23:27:29 +1000

    dweller: import hardware

--- hosts/dweller.nix
+++ hosts/dweller.nix
@@ -23,6 +23,10 @@
   #   }
   # ];
 
+  imports = [
+    ../hardware/dweller.nix
+  ]
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
