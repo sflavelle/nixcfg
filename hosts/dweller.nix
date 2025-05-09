f4a6e14a8fe5fedccab336a182c90a496c3e8de1
Lily Flavelle <me@neurario.com>
2025-05-09 23:17:38 +1000

    dweller: increase download-buffer-size

--- hosts/dweller.nix
+++ hosts/dweller.nix
@@ -35,6 +35,9 @@
 
   # This Chromebook has 128GB storage now, optimise as much as we can
   nix.settings.auto-optimise-store = true;
+  nix.extraOptions = ''
+    download-buffer-size = ${128 * 1024 * 1024}
+    '';
   # Especially memory (2GB)
   zramSwap = {
       enable = true;
