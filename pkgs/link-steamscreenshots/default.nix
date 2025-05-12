32e31308b8bf12dd445a0dffaf8aa24c01b6d593
Lily Flavelle <me@neurario.com>
2025-05-12 10:52:19 +1000

    pkgs/link-steamscreenshots: adjust package for work with a single-file script

--- pkgs/link-steamscreenshots/default.nix
+++ pkgs/link-steamscreenshots/default.nix
@@ -1,12 +1,16 @@
 { lib, python3Packages, ... }:
-stdenv.mkDerivation {
+python3Packages.buildPythonApplication rec {
   pname = "link-steamscreenshots";
-  version = "unstable-2025-05-12"
+  version = "unstable-2025-05-12";
+
+  format = "other";
+
   propagatedBuildInputs = with python3Packages; [
     rich
     requests
   ];
 
+  dontBuild = true;
   dontUnpack = true;
   installPhase = "install -Dm755 ${./${pname}.py} $out/bin/${pname}";
-};
+}
