745318e9262eecfb78f1400d9b399d33ff272284
Lily Flavelle <me@neurario.com>
2025-06-22 16:06:52 +1000

    niri: attempted workaround for shit like discord not loading

--- modules/users/lily/cfg/niri.nix
+++ modules/users/lily/cfg/niri.nix
@@ -84,6 +84,7 @@ lib.mkMerge [
     environment = {
       DISPLAY = ":0";
       NIXOS_OZONE_WL = "1";
+      ELECTRON_DISABLE_GPU = "1"; # https://github.com/NixOS/nixpkgs/issues/382612
       QT_QPA_PLATFORM = "wayland";
       ELECTRON_OZONE_PLATFORM_HINT = "wayland";
     };
