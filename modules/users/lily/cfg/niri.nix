1e7570f7132e86e60d084d22852b848d4f9eed3a
Lily Flavelle <me@neurario.com>
2025-06-28 18:41:23 +1000

    niri: move entire column btw workspaces

--- modules/users/lily/cfg/niri.nix
+++ modules/users/lily/cfg/niri.nix
@@ -205,16 +205,16 @@ lib.mkMerge [
       "Mod+8".action.focus-workspace = 8;
       "Mod+9".action.focus-workspace = 9;
       "Mod+0".action.focus-workspace = 10;
-      "Mod+Shift+1".action.move-window-to-workspace = 1;
-      "Mod+Shift+2".action.move-window-to-workspace = 2;
-      "Mod+Shift+3".action.move-window-to-workspace = 3;
-      "Mod+Shift+4".action.move-window-to-workspace = 4;
-      "Mod+Shift+5".action.move-window-to-workspace = 5;
-      "Mod+Shift+6".action.move-window-to-workspace = 6;
-      "Mod+Shift+7".action.move-window-to-workspace = 7;
-      "Mod+Shift+8".action.move-window-to-workspace = 8;
-      "Mod+Shift+9".action.move-window-to-workspace = 9;
-      "Mod+Shift+0".action.move-window-to-workspace = 10;
+      "Mod+Shift+1".action.move-column-to-workspace = 1;
+      "Mod+Shift+2".action.move-column-to-workspace = 2;
+      "Mod+Shift+3".action.move-column-to-workspace = 3;
+      "Mod+Shift+4".action.move-column-to-workspace = 4;
+      "Mod+Shift+5".action.move-column-to-workspace = 5;
+      "Mod+Shift+6".action.move-column-to-workspace = 6;
+      "Mod+Shift+7".action.move-column-to-workspace = 7;
+      "Mod+Shift+8".action.move-column-to-workspace = 8;
+      "Mod+Shift+9".action.move-column-to-workspace = 9;
+      "Mod+Shift+0".action.move-column-to-workspace = 10;
 
       "Mod+BracketLeft".action.consume-or-expel-window-left = [];
       "Mod+BracketRight".action.consume-or-expel-window-right = [];
