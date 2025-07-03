cd805745bcdf66edc67aea5cb3f9f9320d834d3d
Lily Flavelle <me@neurario.com>
2025-07-03 20:32:33 +1000

    empress: add monitor configuration

--- hosts/empress.nix
+++ hosts/empress.nix
@@ -14,6 +14,17 @@
     hasWifi = true;
     hasBattery = true;
   };
+  monitors = [
+    {
+      name = "eDP-1";
+      primary = true;
+      width = 2560;
+      height = 1600;
+      refreshRate = 60;
+      transform = 90; 
+      scale = 2; # Adjust scale for high DPI displays
+    }
+  ]
 
   imports =
     [ # Include the results of the hardware scan.
@@ -25,27 +36,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Australia/Melbourne";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_AU.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_AU.UTF-8";
-    LC_IDENTIFICATION = "en_AU.UTF-8";
-    LC_MEASUREMENT = "en_AU.UTF-8";
-    LC_MONETARY = "en_AU.UTF-8";
-    LC_NAME = "en_AU.UTF-8";
-    LC_NUMERIC = "en_AU.UTF-8";
-    LC_PAPER = "en_AU.UTF-8";
-    LC_TELEPHONE = "en_AU.UTF-8";
-    LC_TIME = "en_AU.UTF-8";
-  };
   
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
