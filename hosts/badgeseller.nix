40bfbbb102624fba141cbab8890802970249aafd
Lily Flavelle <me@neurario.com>
2025-07-03 14:14:16 +1000

    badgeseller: update with hostspec/monitors, blacklist bt module

--- hosts/badgeseller.nix
+++ hosts/badgeseller.nix
@@ -4,8 +4,24 @@
   hostSpec = {
     hostName = "badgeseller";
     isPublic = true;
-    
+    hasWifi = true;
   };
+  monitors = [
+    {
+      name = "eDP-1";
+      primary = true;
+      width = 2560;
+      height = 1440;
+      refreshRate = 60;
+      x = 0;
+      y = 0;
+      scale = 1.5;
+    }
+  ]
+
+  boot.blacklistedKernelModules = [
+    "hci_bcm4377" # Bluetooth module interferes with the wifi
+  ]
 
   imports =
     [ # Include the results of the hardware scan.
@@ -22,7 +38,6 @@
   };
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "badgeseller"; # Define your hostname.
   networking.networkmanager.wifi.macAddress = "permanent";
   networking.networkmanager.wifi.scanRandMacAddress = false;
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
