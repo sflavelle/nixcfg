1ca7a37c5dcc2e2d1b7b9b0bd615797e7994fb65
Lily Flavelle <me@neurario.com>
2025-07-11 11:29:11 +1000

    use wpa_sup instead of nm for wireless

--- modules/wifi-home.nix
+++ modules/wifi-home.nix
@@ -5,21 +5,12 @@
   ...
 }:
 {
+  networking.networkmanager.unmanaged = [ config.hostSpec.wirelessInterface ];
+
+  networking.wireless.enable = config.hostSpec.wirelessInterface != null;
   networking.wireless.secretsFile = config.age.secrets.FirelinkShrine.path;
-  networking.networkmanager.ensureProfiles.profiles = {
-    home = {
-      connection = {
-        id = "homewifi";
-        type = "wifi";
-      };
-      wifi = {
-        mode = "infrastructure";
-        ssid = "Firelink Shrine";
-      };
-      wifi-security = {
-        key-mgmt = "wpa-psk";
-        pskRaw = "ext:psk_home";
-      };
-    };
-  };
+  networking.wireless.networks = {
+    "FirelinkShrine" = {
+      pskRaw = "ext:psk_home";
+  }
 }
