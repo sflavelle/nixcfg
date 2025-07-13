1b4305dcc148816da12594946d5fc61bb9f87a02
Lily Flavelle <me@neurario.com>
2025-07-13 13:30:45 +1000

    snatcher: disable amdvlk/amdgpu

--- hosts/snatcher.nix
+++ hosts/snatcher.nix
@@ -119,14 +119,14 @@
       enable32Bit = true;
       extraPackages = with pkgs; [
         rocmPackages.clr.icd
-        amdvlk
-        driversi686Linux.amdvlk
+        # amdvlk
+        # driversi686Linux.amdvlk
       ];
     };
-    amdgpu.amdvlk = {
-      enable = true;
-      support32Bit.enable = true;
-    };
+    # amdgpu.amdvlk = {
+    #   enable = true;
+    #   support32Bit.enable = true;
+    # };
   };
 
   systemd.tmpfiles.rules = [
