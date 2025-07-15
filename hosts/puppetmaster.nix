81cda59472f5aca99c4fa7e29677369e2ee176ca
Lily Flavelle <me@neurario.com>
2025-07-15 13:00:01 +1000

    puppetmaster: configure esphome ports

--- hosts/puppetmaster.nix
+++ hosts/puppetmaster.nix
@@ -164,9 +164,10 @@
         "/srv/esphome:/config"
         "/etc/localtime:/etc/localtime:ro"
       ];
-      extraOptions = [
-        "--network=host"
-        # "--device=/dev/ttyUSB0"
+      ports = [ "6052:6052" ]
+        extraOptions = [
+      "--network=host"
+      # "--device=/dev/ttyUSB0"
       ];
     };
     wyoming-whisper = {
