e9ca3de78beff57f34a31eced1811d67026a376a
Lily Flavelle <me@neurario.com>
2025-07-26 14:58:28 +1000

    mpv-watch: update list

--- modules/users/lily/cfg/watch.nix
+++ modules/users/lily/cfg/watch.nix
@@ -48,7 +48,6 @@
     newest_first = true;
     url = "https://www.youtube.com/@ZFG_Vods";
   };
-  RiffTrax = "https://www.twitch.tv/rifftrax";
   "Games Done Quick (VOD)" = {
     shuffle = true;
     url = "https://www.youtube.com/@gamesdonequick";
@@ -59,10 +58,6 @@
   "Summoning Salt" = {
     shuffle = true;
     url = "https://www.youtube.com/playlist?list=PLMxPYcr2zEkWInMyvvxmN22gjrRR0x__u";
-  };
-  "The Detail Geek" = {
-    shuffle = true;
-    url = "https://www.youtube.com/@TheDetailGeek";
   };
   "Game Grumps: Super Mario Sunshine" =
     "https://www.youtube.com/playlist?list=PLRQGRBgN_Enq2ZCxRmlhO0QGHqEFe7VnZ";
@@ -75,12 +70,6 @@
   "Game Grumps: Katamari Series" = {
     shuffle = true;
     url = "https://www.youtube.com/playlist?list=PL14r1zR0h4TZ_rz-fpJR5dQIT8LugMTbt";
-  };
-  "Windows 2000 Professional (SP3).iso" = "https://www.youtube.com/watch?v=M2x9PgNKbN4";
-  "Archlinux-2024.01.01-x86_64.iso" = "https://www.youtube.com/watch?v=aUmaQtTckm0";
-  "Vinesauce (Full Sauce)" = {
-    url = "https://www.youtube.com/@vinesaucefullsauce";
-    newest_first = true;
   };
   "Wristwatch Revival" = "https://www.youtube.com/@WristwatchRevival";
   "Garbage Time" = {
@@ -99,10 +88,6 @@
     url = "https://www.youtube.com/@cryochamberlabel";
     shuffle = true;
   };
-  "(Lofi) Hip Hop" = "https://www.youtube.com/watch?v=jfKfPfyJRdk";
-  "(Lofi) Dark Ambient" = "https://www.youtube.com/watch?v=S_MOd40zlYU";
-  "Datsun 240Z Restoration" =
-    "https://www.youtube.com/playlist?list=PLN0SuqPcbLqGvImGUuOaCsLXTseGiBN02";
   "Simpleflips: Trouble in Terrorist Town" = {
     url = "https://www.youtube.com/playlist?list=PL3Hg1a3VjI1a2rlYY7HPZxWXv2r6rVn-v";
     shuffle = true;
@@ -116,26 +101,16 @@
     url = "https://www.youtube.com/@WirtualTV/videos";
     shuffle = true;
   };
-  "made from dreams" = {
-    url = "https://www.youtube.com/@madefromdream1";
-    shuffle = true;
-  };
   "My Important Videos" = {
     url = "https://www.youtube.com/playlist?list=PLNiFIwkRDS3F1x6iat9gd9wHqT6a379rD";
     shuffle = true;
   };
-  "Midnight Snap" = "https://www.youtube.com/playlist?list=PL8w1FAhwImDz7BEZHJMME_ilqc9CnCuR7";
   "Time Rift Arcade" = {
     url = "https://www.youtube.com/@TimeRiftArcade";
     shuffle = true;
   };
   TASVideosChannels = {
     url = "https://www.youtube.com/@TASVideosChannel";
-    shuffle = true;
-  };
-  LobosJr = {
-    url = "https://www.youtube.com/@LobosjrGaming";
-    remember_progress = true;
     shuffle = true;
   };
   NileRed = {
@@ -150,86 +125,59 @@
     url = "https://www.youtube.com/@lockpickinglawyer";
     shuffle = true;
   };
-  "Metal Gear Scanlon" = {
+  "Giant Bomb: Metal Gear Scanlon" = {
     url = "https://www.youtube.com/playlist?list=PLXlhzeWIuTHLfGvMnnWLJK3DRLQwcTpwd";
     remember_progress = true;
   };
-  "raocow: Paper Mario" = {
-    url = "https://www.youtube.com/playlist?list=PLQhrpbbm5TDLcny_XRHaxV9DpMdVMPab_";
-    remember_progress = true;
-  };
-  "raocow: Super Mario Galaxy" = {
-    url = "https://www.youtube.com/playlist?list=PLQhrpbbm5TDKpx2i6rOgFM_RYJke6L8Li";
-    remember_progress = true;
-  };
-  "raocow: Super Mario Galaxy 2" = {
-    url = "https://www.youtube.com/playlist?list=PLQhrpbbm5TDJ6CxWsN_gQb3EQoweE7XX2";
-    remember_progress = true;
-  };
-  "raocow: Celeste" = {
-    url = "https://www.youtube.com/playlist?list=PLQhrpbbm5TDLuPoeFgmN_vWNIm74dhOJG";
-    remember_progress = true;
-  };
-  "raocow: Half-Life 2 and Episodes" = {
-    url = "https://www.youtube.com/playlist?list=PLQhrpbbm5TDJZRQKT2j_s7Y3FPk6NdGDv";
-    remember_progress = true;
-  };
-  "raocow: VIP 1 (2008)" = {
-    url = "https://www.youtube.com/playlist?list=PLFC4CEB02276B5299";
-    remember_progress = true;
-  };
-  "raocow: VIP 2 (2008)" = {
-    url = "https://www.youtube.com/playlist?list=PL0D0280F96FE9F1FE";
-    remember_progress = true;
-  };
-  "raocow: VIP 3 (2008)" = {
-    url = "https://www.youtube.com/playlist?list=PL96E3A817DDADBA2C";
-    remember_progress = true;
-  };
-  "raocow: VIP 4 (2009)" = {
-    url = "https://www.youtube.com/playlist?list=PL0EA25AC172AC03E8";
-    remember_progress = true;
-  };
-  "Freeman's Mind" = {
-    url = "https://www.youtube.com/playlist?list=PL6PNZBb6b9LvDWpI-5CPYUxG1Rnm-vr9V";
-    remember_progress = true;
-  };
-  "Freeman's Mind 2" = {
-    url = "https://www.youtube.com/playlist?list=PLVA7dP65Vdngocn7xU-pumvpzrrS89_-Y";
-    remember_progress = true;
-  };
-  "Relaxation Ambience" = {
-    url = "https://www.youtube.com/playlist?list=PLNiFIwkRDS3H-A7r3z8v5RlHmwHTZdYKQ";
-    shuffle = true;
-  };
-  "Adrian's Digital Basement" = {
-    url = "https://www.youtube.com/@adriansdigitalbasement";
-    shuffle = true;
-  };
-  "Adrian's Digital Basement ][" = {
-    url = "https://youtube.com/@adriansdigitalbasement2";
-    shuffle = true;
-  };
-  "Is It A Good Idea To Microwave This?" = {
-    url = "https://www.youtube.com/playlist?list=PLU4IMu04MIlJgB6Aaj07q-5iXuHGVzFAR";
-    shuffle = true;
-  };
-  "LVoid's Deltarune Memes" = {
-    url = "https://youtube.com/playlist?list=PLUDzLrZN3WiTX9F7m7mjEyn8W8V-b7P2x";
-    shuffle = true;
-  };
-  "raocow: Cave Story" = {
-    url = "https://www.youtube.com/playlist?list=PLQhrpbbm5TDIhV-m1MWqo2P_VH6DxIHvL";
-    remember_progress = true;
-  };
-  "raocow: Here Comes Niko!" = {
-    url = "https://www.youtube.com/playlist?list=PLQhrpbbm5TDJ-2rgdAuTygvnDU2mIG-pU";
-    remember_progress = true;
-  };
-  "LarsTM: Speedrunning Wampus 1-8, Tompus 1-2, Deep Dip 1-2" =
-    "https://www.youtube.com/watch?v=pqgWgIbC00Q";
-  "Tasty Classics" = {
-    url = "https://youtube.com/@tastyclassics";
-    shuffle = true;
-  };
+  "Giant Bomb: Mario Party Party" =
+    {
+      url = "https://www.youtube.com/playlist?list=PLXlhzeWIuTHLIO-ILN1BPujwqFo_RRP9H";
+      shuffle = true;
+    }
+      "Freeman's Mind" = {
+  url = "https://www.youtube.com/playlist?list=PL6PNZBb6b9LvDWpI-5CPYUxG1Rnm-vr9V";
+  remember_progress = true;
+};
+"Freeman's Mind 2" = {
+url = "https://www.youtube.com/playlist?list=PLVA7dP65Vdngocn7xU-pumvpzrrS89_-Y";
+remember_progress = true;
+};
+"Relaxation Ambience" = {
+url = "https://www.youtube.com/playlist?list=PLNiFIwkRDS3H-A7r3z8v5RlHmwHTZdYKQ";
+shuffle = true;
+};
+"Adrian's Digital Basement" = {
+url = "https://www.youtube.com/@adriansdigitalbasement";
+shuffle = true;
+};
+"Adrian's Digital Basement ][" = {
+url = "https://youtube.com/@adriansdigitalbasement2";
+shuffle = true;
+};
+"Is It A Good Idea To Microwave This?" = {
+url = "https://www.youtube.com/playlist?list=PLU4IMu04MIlJgB6Aaj07q-5iXuHGVzFAR";
+shuffle = true;
+};
+"LVoid's Deltarune Memes" = {
+url = "https://youtube.com/playlist?list=PLUDzLrZN3WiTX9F7m7mjEyn8W8V-b7P2x";
+shuffle = true;
+};
+"LarsTM: Speedrunning Wampus 1-8, Tompus 1-2, Deep Dip 1-2" =
+"https://www.youtube.com/watch?v=pqgWgIbC00Q";
+"Tasty Classics" = {
+url = "https://youtube.com/@tastyclassics";
+shuffle = true;
+};
+"Coestar: Prop Hunt" = {
+url = "https://www.youtube.com/playlist?list=PLEI1-nHBz-afb9TlXVqcKNjp3nIiQjQ2x";
+shuffle = true;
+};
+"Classics of Game" = {
+url = "https://www.youtube.com/@ClassicsOfGame";
+shuffle = true;
+};
+"James Channel" = {
+url = "https://www.youtube.com/@Games_for_James";
+shuffle = true;
+}
 }
