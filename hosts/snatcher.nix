9dc0e33991d7539fcbc7429be653460551c773b2
Lily Flavelle <me@neurario.com>
2025-08-03 14:32:26 +1000

    snatcher: fix deprecated systemd.extraConfig

--- hosts/snatcher.nix
+++ hosts/snatcher.nix
@@ -60,7 +60,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   # boot.kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
-  systemd.extraConfig = "DefaultLimitNOFILE=524288";
+  systemd.settings.Manager = {
+    DefaultLimitNOFILE = 524288;
+  };
 
   networking.firewall.enable = false;
 
