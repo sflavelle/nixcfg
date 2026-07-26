b2619eada411acd264bc5429023b446f2059934f
Lily Flavelle <me@neurario.com>
2026-07-27 01:18:59 +1000

    ap: tried to figure out a thing, didn't wokr

--- modules/features/archipelago.nix
+++ modules/features/archipelago.nix
@@ -2,18 +2,30 @@
 
   flake.nixosModules.archipelago = { config, pkgs, ... }: {
 
-      nixpkgs.overlays = [
-          (final: prev: {
-            archipelago = prev.archipelago.overrideAttrs (oldAttrs: {
-              postInstall = (oldAttrs.postInstall or "") + ''
-                cd $out/lib/python*/site-packages/worlds/
-                rm -rf oot
-                rm -rf tunic.apworld
-                rm -rf celeste_open_world.apworld
-              '';
-            });
-          })
-        ];
+
+      ### THIS DOESN'T WORK
+      ### Basically what's going on is: if you have updates for worlds that are
+      ### in the base Archipelago installation, they won't load. Specifically:
+      ### if they are the old world folder style (OOT for example) they won't
+      ### be updated at all.
+      ### I thought .apworlds would be the same, but despite *indicating* that
+      ### they have the same issue, they do at least load the updated version.
+      ###
+      ### Either way, you can't remove anything from this install (afaik)
+      ### due to permission issues.
+
+      # nixpkgs.overlays = [
+      #     (final: prev: {
+      #       archipelago = prev.archipelago.overrideAttrs (oldAttrs: {
+      #         preInstall = (oldAttrs.preInstall or "") + ''
+      #           cd ${oldAttrs.appimageContents}/opt/Archipelago/lib/worlds/
+
+      #           # chmod -R +w oot tunic.apworld celeste_open_world.apworld
+      #           rm -rf oot
+      #         '';
+      #       });
+      #     })
+      #   ];
 
       environment.systemPackages = with pkgs; [
         archipelago
