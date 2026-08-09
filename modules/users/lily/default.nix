0e715ca607dcd28ffee87864a25454c70b58b6a7
Lily Flavelle <me@neurario.com>
2026-08-09 21:50:43 +1000

    git settings

--- modules/users/lily/default.nix
+++ modules/users/lily/default.nix
@@ -1,6 +1,6 @@
 { self, inputs, ... }: {
 
-  flake.nixosModules.userLily = { pkgs, lib, ... }: {
+  flake.nixosModules.userLily = { pkgs, lib, config, ... }: {
     users.users."lily" = {
       isNormalUser = true;
       description = "Lily Flavelle";
@@ -22,9 +22,47 @@
 
       ];
 
+      programs.git = {
+        enable = true;
+        settings = {
+          user = {
+            name = "Lily Flavelle";
+            email = "me@neurario.com";
+          };
+        };
+      };
+      programs.gh = {
+        enable = true;
+        gitCredentialHelper.enable = true;
+      };
+
       programs.home-manager.enable = true;
 
       programs.zellij.enable = true;
+
+      programs.rclone = {
+        enable = true;
+        remotes = {
+          ndc-files = {
+            config = {
+              type = "webdav";
+              url = "https://files.neurario.com";
+              vendor = "owncloud";
+              user = "splatsune";
+            };
+            mounts."/" = {
+              enable = true;
+              mountPoint = "${config.home-manager.users.lily.home.homeDirectory}/mnt/ndc-files";
+              options = {
+
+              };
+            };
+            # secrets = {
+            #   headers = 
+            # };
+          };
+        };
+      };
     };
   };
 
